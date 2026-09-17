9e3d64523280298ac13bbceba8235d6565099d7f
ArtAttac <sovietbulldozer@gmail.com>
2026-09-17 19:11:22 -0400

    feat: facebook meta integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Outfit } from "next/font/google";
 import localFont from "next/font/local";
+import Script from "next/script";
 import { Analytics } from "@vercel/analytics/next";
 import Navigation from '@/components/Navigation';
 import "./globals.css";
@@ -288,6 +289,29 @@ export default function RootLayout({
         <meta name="theme-color" content="#171e5d" />
       </head>
       <body className={`${inter.variable} ${rawles.variable} ${outfit.variable} font-body`}>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1923384841955068');
+fbq('track', 'PageView');`}
+        </Script>
+        <noscript>
+          {/* Meta requires a plain tracking pixel when JavaScript is disabled. */}
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1923384841955068&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <Navigation />
         {children}
         <Analytics />
